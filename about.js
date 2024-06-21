38d5c65406926ec0038686493e83b94aec8566ba
Naim Abdul Rahman <naimrahman2602@gmail.com>
2024-06-21 23:52:34 +0800

    Fix minor issues
    - Fix dark mode in About
    - Add page for Settings and Tasks

--- about.js
+++ about.js
@@ -7,6 +7,23 @@ document.addEventListener('DOMContentLoaded', function() {
     loadModeFromStorage();
 });
 
+function toggleMenu() {
+    var menu = document.querySelector('.menu-links');
+    menu.classList.toggle('show');
+    document.body.classList.toggle('menu-open'); // Toggle class to darken the background
+
+    // Add event listener to the document
+    document.addEventListener('click', function(event) {
+        var isClickInsideMenu = menu.contains(event.target);
+        var isClickOnMenuIcon = document.querySelector('.menu-icon').contains(event.target);
+
+        if (!isClickInsideMenu && !isClickOnMenuIcon && menu.classList.contains('show')) {
+            menu.classList.remove('show');
+            document.body.classList.remove('menu-open');
+        }
+    });
+}
+
 function toggleMode() {
     document.body.classList.toggle('dark-mode');
     saveModeToStorage();
